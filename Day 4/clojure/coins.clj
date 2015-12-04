3d1edeaf66c93e3a930a0871459402718ecf0121
Kyle Geske <stungeye@gmail.com>
2015-12-04 09:03:04 -0600

    Solved part 2 of Day 4

--- Day 4/clojure/coins.clj
+++ Day 4/clojure/coins.clj
@@ -1,10 +1,11 @@
 (ns coins (:require [digest]))
 
-(defn valid-hash [hash]
-  (= (subs hash 0 5) "00000"))
+(defn valid-hash [hash size]
+  (= (subs hash 0 size)
+     (apply str (repeat size "0"))))
 
-(defn valid-secret [secret]
-  (valid-hash (digest/md5 secret)))
+(defn valid-secret [secret size]
+  (valid-hash (digest/md5 secret) size))
   
-(defn find-secret-number [key]
-  (some #(when (valid-secret (str key %)) %) (range)))
+(defn find-secret-number [key size]
+  (some #(when (valid-secret (str key %) size) %) (range)))
--- Day 4/clojure/coins_test.clj
+++ Day 4/clojure/coins_test.clj
@@ -3,23 +3,35 @@
 
 (load-file "coins.clj")
 
-(deftest valid-hash
-  (is (= true (coins/valid-hash "000001dbbfa"))))
+(deftest valid-hash-a
+  (is (= true (coins/valid-hash "000001dbbfa" 5))))
 
-(deftest invalid-hash
-  (is (= false (coins/valid-hash "000021dbbfa"))))
+(deftest valid-hash-b
+  (is (= true (coins/valid-hash "000000dbbfa" 6))))
+  
+(deftest valid-hash-c
+  (is (= true (coins/valid-hash "000021dbbfa" 4))))
+  
+(deftest invalid-hash-a
+  (is (= false (coins/valid-hash "000021dbbfa" 5))))
+  
+(deftest invalid-hash-b
+  (is (= false (coins/valid-hash "000001dbbfa" 6))))
 
 (deftest valid-secret
-  (is (= true (coins/valid-secret "abcdef609043"))))
+  (is (= true (coins/valid-secret "abcdef609043" 5))))
 
 (deftest invalid-secret
-  (is (= false (coins/valid-secret "Kbcdef609043"))))
+  (is (= false (coins/valid-secret "Kbcdef609043" 5))))
 
 (deftest find-secret-a
-  (is (= 609043 (coins/find-secret-number "abcdef"))))
+  (is (= 609043 (coins/find-secret-number "abcdef" 5))))
   
 (deftest find-secret-b
-  (is (= 1048970 (coins/find-secret-number "pqrstuv"))))
+  (is (= 1048970 (coins/find-secret-number "pqrstuv" 5))))
 
 (deftest challenge-1
-  (is (= 254575 (coins/find-secret-number "bgvyzdsv"))))
+  (is (= 254575 (coins/find-secret-number "bgvyzdsv" 5))))
+  
+(deftest challenge-2
+  (is (= 254575 (coins/find-secret-number "bgvyzdsv" 6))))
