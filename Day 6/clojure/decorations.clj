364c20c6b4ff7c15d09df0894ee2e15e4d67440d
Kyle Geske <stungeye@gmail.com>
2015-12-10 16:17:30 -0600

    Light toggling.

--- Day 6/clojure/decorations.clj
+++ Day 6/clojure/decorations.clj
@@ -1,14 +1,6 @@
 (ns decorations (:use clojure.set))
 
 (def tokenizer #"(turn on|turn off|toggle) (\d{1,3}),(\d{1,3}) through (\d{1,3}),(\d{1,3})")
-(def width 1000)
-(def height 1000)
-
-(def initial-grid
-  (vec (repeat (* width height) false)))
-
-(defn point-to-position [x y width height]
-  (+ x (* y width)))
 
 (defn tokenize-command [string]
   (let [[_ command & coords] (re-matches tokenizer string)]
@@ -25,8 +17,17 @@
   (let [switch-off (into #{} (for [x x-range y y-range] [x y]))]
     (clojure.set/difference lights switch-off)))
 
+(defn toggle-lights [lights x-range y-range]
+  (let [to-toggle (into #{} (for [x x-range y y-range] [x y]))
+        switch-on (clojure.set/difference to-toggle lights)
+        switch-off (clojure.set/difference to-toggle switch-on)]
+    (clojure.set/union switch-on (clojure.set/difference lights switch-off))))
+
 (defn next-light-set [lights command]
   (let [[action x1 y1 x2 y2] command
         x-range (range x1 (inc x2))
         y-range (range y1 (inc y2))]
-    (turn-lights-on lights x-range y-range)))
+    (case action
+      "turn on" (turn-lights-on lights x-range y-range)
+      "turn off" (turn-lights-off lights x-range y-range)
+      "toggle" (toggle-lights lights x-range y-range))))
--- Day 6/clojure/decorations_test.clj
+++ Day 6/clojure/decorations_test.clj
@@ -3,24 +3,6 @@
 
 (load-file "decorations.clj")
 
-(deftest correctly-builds-initial-grid
-  (is (= 1000000 (count decorations/initial-grid))))
-
-(deftest converts-points-to-positions-1
-  (is (= 0 (decorations/point-to-position 0 0 10 10))))
-
-(deftest converts-points-to-positions-2
-  (is (= 1 (decorations/point-to-position 1 0 10 10))))
-
-(deftest converts-points-to-positions-3
-  (is (= 10 (decorations/point-to-position 0 1 10 10))))
-
-(deftest converts-points-to-positions-4
-  (is (= 11 (decorations/point-to-position 1 1 10 10))))
-
-(deftest converts-points-to-positions-5
-  (is (= 99 (decorations/point-to-position 9 9 10 10))))
-  
 (deftest tokenize-command
   (is (= ["turn on" 489 959 759 964] (decorations/tokenize-command "turn on 489,959 through 759,964"))))
 
@@ -33,6 +15,12 @@
 (deftest next-light-set-1
   (is (= 9 (count (decorations/next-light-set #{} ["turn on" 0 0 2 2])))))
 
+(deftest next-light-set-2
+  (is (= 5 (count (decorations/next-light-set nine-lights-on ["turn off" 0 0 1 1])))))
+
+(deftest next-light-set-3
+  (is (= 11 (count (decorations/next-light-set nine-lights-on ["toggle" 2 2 3 3])))))
+
 (deftest turn-lights-on-1
   (is (= 9 (count (decorations/turn-lights-on #{} (range 0 3) (range 0 3))))))
 
@@ -42,3 +30,12 @@
 
 (deftest turn-lights-off-1
   (is (= 5 (count (decorations/turn-lights-off nine-lights-on (range 0 2) (range 0 2))))))
+
+(deftest toggle-lights-1
+  (is (= 0 (count (decorations/toggle-lights nine-lights-on (range 0 3) (range 0 3))))))
+  
+(deftest toggle-lights-2
+  (is (= 9 (count (decorations/toggle-lights #{} (range 0 3) (range 0 3))))))
+  
+(deftest toggle-lights-3
+  (is (= 11 (count (decorations/toggle-lights nine-lights-on (range 2 4) (range 2 4))))))
