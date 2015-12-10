680463e1a6f73880b79fd1b12ebf3517dd95013f
Kyle Geske <stungeye@gmail.com>
2015-12-10 15:47:32 -0600

    Turning lights on and off.

--- Day 6/clojure/decorations.clj
+++ Day 6/clojure/decorations.clj
@@ -1,4 +1,4 @@
-(ns decorations)
+(ns decorations (:use clojure.set))
 
 (def tokenizer #"(turn on|turn off|toggle) (\d{1,3}),(\d{1,3}) through (\d{1,3}),(\d{1,3})")
 (def width 1000)
@@ -16,4 +16,17 @@
 
 (defn tokenize-file [filename]
   (let [raw-commands (clojure.string/split (slurp filename) #"\r\n")]
-    (map tokenize-command raw-commands)))  
+    (map tokenize-command raw-commands)))
+
+(defn turn-lights-on [lights x-range y-range]
+  (into lights (for [x x-range y y-range] [x y])))
+
+(defn turn-lights-off [lights x-range y-range]
+  (let [switch-off (into #{} (for [x x-range y y-range] [x y]))]
+    (clojure.set/difference lights switch-off)))
+
+(defn next-light-set [lights command]
+  (let [[action x1 y1 x2 y2] command
+        x-range (range x1 (inc x2))
+        y-range (range y1 (inc y2))]
+    (turn-lights-on lights x-range y-range)))
--- Day 6/clojure/decorations_test.clj
+++ Day 6/clojure/decorations_test.clj
@@ -27,3 +27,18 @@
 (deftest tokenize-from-file
   (is (= [["turn on" 489 959 759 964] ["turn off" 820 516 871 914]]
          (decorations/tokenize-file "test_commands.txt"))))
+
+(def nine-lights-on #{[0 0] [0 1] [0 2] [1 0] [1 1] [1 2] [2 0] [2 1] [2 2]})
+
+(deftest next-light-set-1
+  (is (= 9 (count (decorations/next-light-set #{} ["turn on" 0 0 2 2])))))
+
+(deftest turn-lights-on-1
+  (is (= 9 (count (decorations/turn-lights-on #{} (range 0 3) (range 0 3))))))
+
+(deftest turn-lights-on-2
+  (is (= nine-lights-on
+         (decorations/turn-lights-on #{} (range 0 3) (range 0 3)))))
+
+(deftest turn-lights-off-1
+  (is (= 5 (count (decorations/turn-lights-off nine-lights-on (range 0 2) (range 0 2))))))
