1d20148d932522c2db278d634418d5496ab26ae5
Kyle Geske <stungeye@gmail.com>
2015-12-16 13:59:11 -0600

    Solution to day 8 part 1.

--- Day 8/clojure/characters.clj
+++ Day 8/clojure/characters.clj
@@ -2,3 +2,17 @@
 
 (defn file->strings [filename]
   (clojure.string/split (slurp filename) #"\r\n"))
+
+(defn in-memory-count [string]
+  (-> string
+      (clojure.string/replace #"^\"(.*)\"$" "$1")
+      (clojure.string/replace #"\\\"" "_")
+      (clojure.string/replace #"\\\\" "_")
+      (clojure.string/replace #"\\x.." "_")
+      count))
+
+(defn char-count-diff [string]
+  (- (count string) (in-memory-count string)))
+
+(defn char-count-diff-collection [strings]
+  (reduce + (map char-count-diff strings)))
--- Day 8/clojure/characters_test.clj
+++ Day 8/clojure/characters_test.clj
@@ -3,5 +3,39 @@
 
 (load-file "characters.clj")
 
-(deftest test-test
-  (is (= 1 1)))
+(def test-strings (characters/file->strings "test_strings.txt"))
+
+(deftest literal-count-empty-string
+  (is (= 2 (count (test-strings 0)))))
+
+(deftest literal-count-string
+  (is (= 5 (count (test-strings 1)))))
+
+(deftest literal-count-string-with-escaped-quote
+  (is (= 10 (count (test-strings 2)))))
+
+(deftest literal-count-string-with-hex-escape
+  (is (= 6 (count (test-strings 3)))))
+
+(deftest in-memory-count-empty-string
+  (is (= 0 (characters/in-memory-count (test-strings 0)))))
+
+(deftest in-memory-count-string
+  (is (= 3 (characters/in-memory-count (test-strings 1)))))
+
+(deftest in-memory-count-string-with-escaped-quote
+  (is (= 7 (characters/in-memory-count (test-strings 2)))))
+
+(deftest in-memory-count-string-with-hex-escape
+  (is (= 1 (characters/in-memory-count (test-strings 3)))))
+
+(deftest char-count-diff-1
+  (is (= 5 (characters/char-count-diff (test-strings 3)))))
+
+(deftest char-count-diff-collection-test
+  (is (= 12 (characters/char-count-diff-collection test-strings))))
+
+(def challenge-strings (characters/file->strings "strings.txt"))
+
+(deftest challenge-1
+  (is (= 1350 (characters/char-count-diff-collection challenge-strings))))
